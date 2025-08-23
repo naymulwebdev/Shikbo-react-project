b8d431c184452607c120189fb9f1f4b4040c88e6
naymulislam2000 <naim94656@gmail.com>
2025-08-23 16:54:24 +0300

    sceond commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  
   return (
     <div>
-      <h1>hello world!</h1>
+      <h1 className='bg-green-600'>hello world!</h1>
     </div>
   )
 }
